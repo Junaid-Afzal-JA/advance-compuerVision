8a50cf27b5a9f325d76630eb8db165eab0ab9973
Junaid Afzal <junaid729710211597@gmail.com>
2021-12-27 00:08:30 +0500

    fixing issue in HandDetector and startig gesture volume control

---
+++ gesture_volume_control.py
@@ -0,0 +1,34 @@
+import cv2
+from hand_tracking.hand_tracking import HandDetector
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(0)
+    detect = HandDetector()
+
+    while True:
+        _, image = cap.read()
+        if _:
+            detect.find_hands(image, draw=True)
+            marks = detect.find_position(image)
+            if len(marks) > 0:
+                thumb = (marks[8][1], marks[8][2])
+                finger = (marks[4][1], marks[4][2])
+                cv2.circle(img=image,
+                           center=thumb, radius=10,
+                           color=(0, 0, 0),
+                           thickness=cv2.FILLED,
+                           )
+                cv2.circle(img=image,
+                           center=finger, radius=10,
+                           color=(0, 0, 0),
+                           thickness=cv2.FILLED
+                           )
+                cv2.line(img=image,
+                         pt1=thumb,
+                         pt2=finger,
+                         color=(255, 0, 0),
+                         thickness=3
+                         )
+            cv2.imshow('image', image)
+
+            cv2.waitKey(1)
--- hand_tracking/hand_tracking.py
+++ hand_tracking/hand_tracking.py
@@ -23,7 +23,7 @@ class HandDetector:
         self.temp_image = None
         self.draw = mp.solutions.drawing_utils
 
-    def find_hands(self, image, draw=False):
+    def find_hands(self, image, draw=True):
         """
         Message:
             Take image in BGR and draw landmarks of hands on it.
@@ -43,7 +43,7 @@ class HandDetector:
 
         return image
 
-    def find_position(self, hand_no=0, draw=True):
+    def find_position(self, image, hand_no=0, draw=False):
         """
         Message:
             Return landmarks position of hands
@@ -52,56 +52,21 @@ class HandDetector:
             hand_no: Which hand to identify
             draw: Draw landmark or not.
         """
-        # land_marks = []
-        if self.result.multi_hand_landmarks:
-            specific_hand = self.result.multi_hand_landmarks[hand_no]
+        height, width, channel = image.shape
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        result = self.my_hands.process(rgb_image)
+        land_marks = []
+        if result.multi_hand_landmarks:
+            specific_hand = result.multi_hand_landmarks[hand_no]
             for mark_no, mark in enumerate(specific_hand.landmark):
                 x_point = int(mark.x * width)
                 y_point = int(mark.y * height)
-                # land_marks.append([mark_no, x_point, y_point])
+                land_marks.append([mark_no, x_point, y_point])
                 if draw:
-                    cv2.circle(img=self.temp_image,
+                    cv2.circle(img=image,
                                center=(x_point, y_point), radius=8,
                                color=(210, 50, 50),
-                               thickness=2)
+                               thickness=2
+                               )
 
-        # return land_marks
-
-
-if __name__ == '__main__':
-    cam = cv2.VideoCapture(0)
-    previous_time = 0
-    while True:
-        more, frame = cam.read()
-        height, width, channel = frame.shape
-        detect = HandDetector(model_complexity=0)
-        res_image = detect.find_hands(frame, draw=True)
-        marks = detect.find_position(draw=False)
-        # if len(marks) > 0:
-        #     print((marks[8]))
-
-        #
-        # rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        #
-        # result = my_hands.process(rgb_image)
-        # print(result.multi_hand_landmarks)
-
-        current_time = time.time()
-        # if result.multi_hand_landmarks:
-        #     for hands in result.multi_hand_landmarks:
-        #         draw.draw_landmarks(frame, hands, MpHands.HAND_CONNECTIONS)
-        #         for no, mark in enumerate(hands.landmark):
-        #             x_point = int(mark.x*width)
-        #             y_point = int(mark.y*height)
-        #             if no % 4 ==0:
-        #                 cv2.circle(img=frame,
-        #                            center=(x_point, y_point), radius=8,
-        #                            color=(210, 50, 50),
-        #                            thickness=2)
-
-        fps = 1 / (current_time - previous_time)
-        previous_time = current_time
-
-        cv2.putText(res_image, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (210, 100, 100), 3)
-        cv2.imshow('live', res_image)
-        cv2.waitKey(1)
+        return land_marks
